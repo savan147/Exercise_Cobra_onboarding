9775dd3664e1fb4c203e63f6fe3e63125b290218
savan147 <11mca147@srimca.edu.in>
2017-08-19 21:08:51 +1200

    Add project files.

---
+++ Exercise_Cobra_onboarding/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Exercise_Cobra_onboarding.MvcApplication" Language="C#" %>
---
+++ Exercise_Cobra_onboarding/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Exercise_Cobra_onboarding.Startup))]
+namespace Exercise_Cobra_onboarding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
